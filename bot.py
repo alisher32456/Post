f2c59f82292363e88002acc49fa23807ba65cf30
alisher32456 <mralisher567@gmail.com>
2025-07-15 09:39:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,16 @@
-import os
-import requests
-import random
-
-BOT_TOKEN = os.getenv("7572960990:AAHyRBXvcEs1f5S5pfx-gkOUADw598L8668")
-CHANNEL = os.getenv("@Tech_Pulse_Apps_AI_Hacks")
-
-def get_random_post():
-    with open("posts.txt", "r", encoding="utf-8") as f:
-        posts = f.read().strip().split("\n\n")
-        return random.choice(posts)
-
 def send_to_telegram(text):
+    import os, requests
+    BOT_TOKEN = "7572960990:AAHyRBXvcEs1f5S5pfx-gkOUADw598L8668"
+    CHANNEL = "@Tech_Pulse_Apps_AI_Hacks"
+
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {
         "chat_id": CHANNEL,
         "text": text,
         "parse_mode": "HTML"
     }
-    r = requests.post(url, data=data)
-    print(f"Status: {r.status_code}, Response: {r.text}")
 
-if __name__ == "__main__":
-    post = get_random_post()
-    send_to_telegram(post)
-  
+    response = requests.post(url, data=data)
+    print(f"Status: {response.status_code}, Response: {response.text}")
+
+send_to_telegram("✅ Test message from your GitHub bot!\n👉 @Tech_Pulse_Proxy_MTProto")
